07e03b4fabe906586a8538c7c1ad4192da2907d8
wilko <wilkorosendaal@gmail.com>
2025-09-30 15:28:52 +0200

    working input validation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def input_validation(inp) -> bool:
+    for c in inp:
+        if c.isdigit() or c.isalpha():
+            continue
+        else:
+            return False
     return True
 
 
@@ -6,21 +11,21 @@ def main_menu_input_handler(inp):
     inp = inp.lower()
     if not input_validation(inp):
         main_menu_input_handler(
-            input("please dont use any spaces or special charcters")
+            input("please dont use any spaces or special charcters\n")
         )
     match inp:
         case "q":
-            print("quiting")
+            print("quiting\n")
             return "q"
         case _:
             main_menu_input_handler(input
-                                    ("please pick on of our optiions")
+                                    ("please pick on of our optiions\n")
                                     )
 
 
 def main():
     while True:
-        test = main_menu_input_handler(input("pick a input"))
+        test = main_menu_input_handler(input("pick a input\n"))
         if test == "q":
             break
 
