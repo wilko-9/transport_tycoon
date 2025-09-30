42032c734dcdd256a33d86155aa250899530b604
wilko <wilkorosendaal@gmail.com>
2025-09-30 15:40:44 +0200

    fixed a bug

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def input_validation(inp) -> bool:
     return True
 
 
-def main_menu_input_handler(inp):
+def main_menu_input_handler(inp) -> str:
     inp = inp.lower()
     if not input_validation(inp):
         main_menu_input_handler(
@@ -18,9 +18,10 @@ def main_menu_input_handler(inp):
             print("quiting\n")
             return "q"
         case _:
-            main_menu_input_handler(input
-                                    ("please pick on of our optiions\n")
-                                    )
+            inp = main_menu_input_handler(input
+                                          ("please pick on of our optiions\n")
+                                          )
+            return inp
 
 
 def main():
